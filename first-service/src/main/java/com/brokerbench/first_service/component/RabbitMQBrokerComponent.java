bb926803558642f4aeb453211b85967241275c41
Prajna Jeet Ojha <97689950+Prajna0106@users.noreply.github.com>
2025-11-14 23:03:05 +0530

    Modified RabbitMQ Config

--- first-service/src/main/java/com/brokerbench/first_service/component/RabbitMQBrokerComponent.java
+++ first-service/src/main/java/com/brokerbench/first_service/component/RabbitMQBrokerComponent.java
@@ -2,6 +2,7 @@ package com.brokerbench.first_service.component;
 
 import com.brokerbench.first_service.dto.Message;
 import com.brokerbench.first_service.enums.BrokerType;
+import com.brokerbench.first_service.service.RabbitMQProducer;
 import com.brokerbench.first_service.strategy.BrokerTypeStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,12 @@ public class RabbitMQBrokerComponent implements BrokerTypeStrategy {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private RabbitMQProducer rabbitMQProducer;
+
+    public RabbitMQBrokerComponent(RabbitMQProducer rabbitMQProducer) {
+        this.rabbitMQProducer = rabbitMQProducer;
+    }
+
     @Override
     public BrokerType getBrokerType() {
         return BrokerType.RABBITMQ;
@@ -19,7 +26,8 @@ public class RabbitMQBrokerComponent implements BrokerTypeStrategy {
 
     @Override
     public void sendMessage(Message message) {
-        // TO DO -- SEND KAFKA MESSAGE
+        // TO DO -- SEND RABBITMQ MESSAGE
+        rabbitMQProducer.sendMessage(message.getMessage());
         logger.info("MESSAGE RECEIVED FOR RABBITMQ   ---- " + message.getMessage());
     }
 
