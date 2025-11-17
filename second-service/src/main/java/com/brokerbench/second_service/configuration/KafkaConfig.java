d3c5bdf8b244c5a343b35ff869e62f458ed7bc51
Rohan Mohanty <mohantyrohan3@gmail.com>
2025-11-17 23:20:56 +0530

    Updated Kafka Config

--- second-service/src/main/java/com/brokerbench/second_service/configuration/KafkaConfig.java
+++ second-service/src/main/java/com/brokerbench/second_service/configuration/KafkaConfig.java
@@ -1,20 +1,22 @@
 package com.brokerbench.second_service.configuration;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
-    @Value("${spring.kafka.consumer.topic-name}")
-    private String topicName;
-
-    @Bean
-    public NewTopic newTopic() {
-        return TopicBuilder.name(topicName)
-                .build();
-    }
 }
