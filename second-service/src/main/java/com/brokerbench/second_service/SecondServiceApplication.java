5e43e1b5a213f6134078a6b86b33ecfe411baa5d
Rohan Mohanty <mohantyrohan3@gmail.com>
2025-11-22 15:44:40 +0530

    Updated the Application properties

--- second-service/src/main/java/com/brokerbench/second_service/SecondServiceApplication.java
+++ second-service/src/main/java/com/brokerbench/second_service/SecondServiceApplication.java
@@ -3,9 +3,11 @@ package com.brokerbench.second_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableKafka
 public class SecondServiceApplication {
 
 	public static void main(String[] args) {
